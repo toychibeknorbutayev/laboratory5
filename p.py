a08f1be0b0ed9cf9d61facd13d0d0c061f69f975
toychibeknorbutayev <125568491+toychibeknorbutayev@users.noreply.github.com>
2023-06-01 21:25:02 +0300

    Update p.py

--- p.py
+++ p.py
@@ -5,7 +5,7 @@ import glob
 chessboardSize = (10, 14)
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
-objp[:, :2] = np.mgrid[0:chessboardSize[0]
+objp[:, :2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]]
 prev_img_shape = None
 
 objpoints = []
