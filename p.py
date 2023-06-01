a12001af36f4310f54a88507b0db44bffefc7c37
toychibeknorbutayev <125568491+toychibeknorbutayev@users.noreply.github.com>
2023-06-01 21:22:59 +0300

    Update p.py

--- p.py
+++ p.py
@@ -5,18 +5,18 @@ import glob
 chessboardSize = (10, 14)
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
-objp[:, :2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1, 2)
+objp[:, :2] = np.mgrid[0:chessboardSize[0]
 prev_img_shape = None
 
 objpoints = []
-imgpoints = [] 
+imgpoints = []
 images = glob.glob('*.jpg')
 for image in images:
     print(image)
     img = cv.imread(image)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     ret, corners = cv.findChessboardCorners(gray, chessboardSize)
-    if ret == True:
+    if ret = True:
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners)
